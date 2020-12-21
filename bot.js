2e970bb220ce4bcdd8de2477e249b2af75af415e
reneald <leander@leanderonline.be>
2020-12-21 20:54:45 +0100

    added addBand functionality

--- bot.js
+++ bot.js
@@ -7,6 +7,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 
+const bandNames = [
+    'Toehider',
+    'Rush',
+    'Evergrey',
+    'Symphony X',
+    'Ayreon',
+    'Pain of Salvation',
+    'Dream Theater'
+]
+
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
@@ -17,8 +27,38 @@ client.on('message', gotMessage);
 
 function gotMessage(message) {
     console.log(message.content);
-    if (message.channel.id == process.env.BOT_TESTING_CHANNEL_ID &&
-        message.content === 'prog rocks') {
-        message.reply('no doubt');
+    if (message.channel.id == process.env.BOT_TESTING_CHANNEL_ID) {
+        if (message.content.match(/!.+/)) {
+            const command = message.content.match(/(!\w+)/)[1];
+            switch (command) {
+                case "!band":
+                    replyBandName(message);
+                    break;
+                case "!addband":
+                    addBand(message.content);
+                    break;
+                default:
+                    break;
+            }
+        } else if (message.content === 'prog rocks') {
+            noDoubt(message);
+        }
+        
+
     }
+}
+
+function replyBandName(message) {
+    const index = Math.floor(Math.random() * bandNames.length);
+    message.channel.send(bandNames[index]);
+}
+
+function noDoubt(message) {
+    message.reply('no doubt');
+}
+
+function addBand(messageContent) {
+    const separateCommandFromContent = messageContent.match(/!\w+ (.+)/);
+    const bandName = separateCommandFromContent[1];
+    bandNames.push(bandName);
 }
