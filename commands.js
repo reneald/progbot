667869d2f69e8c84b4fa372b09494cda448527a1
reneald <leander@leanderonline.be>
2020-12-22 11:49:51 +0100

    some cleanup

--- commands.js
+++ commands.js
@@ -1,27 +1,35 @@
 import { readFile } from 'fs';
 
-const showHelp = function showHelp(message) {
+const showHelp = function (message) {
+    console.log('Message ' + message.id + ' is a cry for help...');
     readFile('HELP.md','utf-8', (error, data) => {
         if (error) console.log(error);
         message.channel.send(data);
     });
+    console.log('Cry answered. Processing message ' + message.id + ' complete.');
 }
 
-const replyBandName = function replyBandName(message) {
+const replyBandName = function (message) {
+    console.log('Message ' + message.id + ' asks for a band name...');
     const index = Math.floor(Math.random() * bandNames.length);
     message.channel.send(bandNames[index]);
+    console.log('Provided band name. Processing message ' + message.id + ' complete.');
 }
 
-const noDoubt = function noDoubt(message) {
-    message.reply('no doubt');
-}
-
-const addBand = function addBand(messageContent) {
-    const separateCommandFromContent = messageContent.match(/!\w+ (.+)/);
+const addBand = function (message) {
+    console.log('Message ' + message.id + ' wants to add a band to my list...');
+    const separateCommandFromContent = message.content.match(/!\w+ (.+)/);
     const bandName = separateCommandFromContent[1];
     bandNames.push(bandName);
+    console.log('Band name added. Processing message ' + message.id + ' complete.');
+}
+
+const noDoubt = function (message) {
+    console.log('Message ' + message.id + ' tells the truth...');
+    message.reply('no doubt');
+    console.log('Is No Doubt prog? Processing message ' + message.id + ' complete.');
 }
 
 export {
-    showHelp, replyBandName, noDoubt, addBand
+    showHelp, replyBandName, addBand, noDoubt
 }
