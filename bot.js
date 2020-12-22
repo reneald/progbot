93a32b5216103ad93de36f7249392f4e1d22c6b3
reneald <leander@leanderonline.be>
2020-12-22 11:27:35 +0100

    removing obsolete code

--- bot.js
+++ bot.js
@@ -50,25 +50,3 @@ function gotMessage(message) {
 
     }
 }
-
-// function showHelp(message) {
-//     readFile('HELP.md','utf-8', (error, data) => {
-//         if (error) console.log(error);
-//         message.reply(data);
-//     });
-// }
-
-// function replyBandName(message) {
-//     const index = Math.floor(Math.random() * bandNames.length);
-//     message.channel.send(bandNames[index]);
-// }
-
-// function noDoubt(message) {
-//     message.reply('no doubt');
-// }
-
-// function addBand(messageContent) {
-//     const separateCommandFromContent = messageContent.match(/!\w+ (.+)/);
-//     const bandName = separateCommandFromContent[1];
-//     bandNames.push(bandName);
-// }
