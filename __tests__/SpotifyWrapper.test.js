e8c22a1099d6e89df1020315ece8c3a9933772d0
reneald <leander@leanderonline.be>
2021-01-10 20:36:50 +0100

    trying to test error message from spotifywebapi

--- __tests__/SpotifyWrapper.test.js
+++ __tests__/SpotifyWrapper.test.js
@@ -21,4 +21,28 @@ describe("SpotifyWrapper", () => {
             expect(data).toBe(expectedQuery);
         })
     })
+
+    //TODO fix this test
+    // test('searchArtist() should log the error body if an error occurs', () => {
+    //     //GIVEN
+    //     const query = 'Tool';
+    //     const error = {
+    //         "status": 401,
+    //         "message": "invalid access token"
+    //     }
+    //     const spy = jest.spyOn(SpotifyWebApi, 'searchArtists').mockImplementation(() => {
+        
+    //             return new Promise((resolve, reject) => {
+    //                 reject(error);
+    //             });
+            
+    //     })
+
+    //     //WHEN/THEN
+    //     expect.assertions(1);
+    //     return spotifyWrapper.searchArtist(query).then(data => {
+    //         expect(data).toBe(error);
+    //     })
+
+    // })
 })
